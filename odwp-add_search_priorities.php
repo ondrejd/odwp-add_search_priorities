db80c0581ee393d4f36da1138916f49172c98b7f
Ondřej Doněk <ondrejd@gmail.com>
2018-04-24 17:27:17 +0200

    Fixed issue #6

--- odwp-add_search_priorities.php
+++ odwp-add_search_priorities.php
@@ -27,6 +27,7 @@ if( ! defined( 'ABSPATH' ) ) {
 
 defined( 'ODWPASP_ADMIN_PAGE' ) || define( 'ODWPASP_ADMIN_PAGE', 'odwpasp-admin_page' );
 defined( 'ODWPASP_META_KEY' ) || define( 'ODWPASP_META_KEY', 'odwpasp-priority' );
+defined( 'ODWPASP_FILE' ) || define( 'ODWPASP_FILE', basename( __FILE__ ) );
 
 
 if( !function_exists( 'odwpasp_admin_enqueue_scripts' ) ) :
@@ -35,6 +36,10 @@ if( !function_exists( 'odwpasp_admin_enqueue_scripts' ) ) :
      * @param string $hook
      * @return void
      * @since 1.0.0
+     * @uses plugins_url()
+     * @uses wp_enqueue_script()
+     * @uses wp_enqueue_style()
+     * @uses wp_localize_script()
      */
     function odwpasp_admin_enqueue_scripts( $hook ) {
         $js_file = 'assets/js/admin.js';
@@ -63,6 +68,7 @@ if( !function_exists( 'odwpasp_add_admin_page' ) ) :
      * Adds our admin page.
      * @return void
      * @since 1.0.0
+     * @uses add_submenu_page()
      */
     function odwpasp_add_admin_page() {
         add_submenu_page(
@@ -84,28 +90,40 @@ if( !function_exists( 'odwpasp_render_admin_page' ) ) :
      * @link https://wpdreams.gitbooks.io/ajax-search-pro-documentation/content/priority_settings/individual-priorities.html - ukázka řešení
      * @return void
      * @since 1.0.0
-     * @todo Add _NONCE field!
-     * @todo Sanitize search term from GET/POST!
      * @todo Remove all inline styles and move them into proper CSS file!
+     * @uses current_user_can()
+     * @uses sanitize_text_field()
+     * @uses wp_die()
+     * @uses wp_nonce_field()
+     * @uses wp_verify_nonce()
+     * @uses update_post_meta()
      */
     function odwpasp_render_admin_page() {
         if ( !current_user_can( 'manage_options' ) )  {
             wp_die( __( 'You have incuffient permissions for accessing this page.', 'odwpasp' ) );
         }
 
-        $term = isset( $_GET['term'] ) ? $_GET['term'] : ( isset( $_POST['term'] ) ? $_POST['term'] : '' );
+        $term = isset( $_GET['term'] ) ? sanitize_text_field( $_GET['term'] ) : ( isset( $_POST['term'] ) ? sanitize_text_field( $_POST['term'] ) : '' );
         $post_status = isset( $_GET['post_status'] ) ? $_GET['post_status'] : ( isset( $_POST['post_status'] ) ? $_POST['post_status'] : '' );
         $submitted_priorities = isset( $_POST['submit_priorities'] );
         $submitted_search = ( isset( $_GET['submit_search'] ) || $submitted_priorities );
+        $is_valid_nonce = false;
         $query = null;
 
         if( $submitted_priorities ) {
+            $is_valid_nonce = ( isset( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'odwpasp_nonce' ) );
+        }
+        elseif( $submitted_search ) {
+            $is_valid_nonce = ( isset( $_GET['_wpnonce'] ) && wp_verify_nonce( $_GET['_wpnonce'], 'odwpasp_nonce' ) );
+        }
+        
+        if( $submitted_priorities && $is_valid_nonce ) {
             foreach( $_POST['p'] as $id => $v ) {
                 update_post_meta( $id, ODWPASP_META_KEY, $v );
             }
         }
 
-        if( $submitted_search ) {
+        if( $submitted_search && $is_valid_nonce ) {
             $args = array(
                 's' => $term,
                 'post_type' => ['page', 'post'],
@@ -133,10 +151,16 @@ if( !function_exists( 'odwpasp_render_admin_page' ) ) :
 <div class="wrap odwpasp">
     <h1 class="wp-heading-inline"><?php _e( 'Customize search results order', 'odwpasp' ) ?></h1>
     <hr class="wp-header-end">
+    <?php if( ( $submitted_priorities || $submitted_search ) && !$is_valid_nonce ) : ?>
+    <div class="notice notice-error s-dismissible">
+        <p><?php _e( 'Request was not processed correctly, something wrong with security&hellip;', 'odwpasp' ) ?></p>
+    </div>
+    <?php endif ?>
     <div class="card">
         <h2 class="title" style="margin-bottom:0;"><?php _e( 'Test search', 'odwpasp' ) ?></h2>
         <form action="" method="GET">
             <input type="hidden" name="page" value="<?php echo ODWPASP_ADMIN_PAGE ?>">
+            <?php wp_nonce_field( 'odwpasp_nonce' ) ?>
             <span class="inline-input">
                 <label for="odwpasp-search_term"><?php _e( 'Search term: ', 'odwpasp' ) ?></label>
                 <input class="regular-text" id="odwpasp-search_term" name="term" type="text" value="<?php esc_attr( $term ) ?>">
@@ -151,7 +175,7 @@ if( !function_exists( 'odwpasp_render_admin_page' ) ) :
                 </select>
             </span>
             <input id="odwpasp-search_submit_btn" name="submit_search" type="submit" value="<?php esc_attr_e( 'Search', 'odwpasp' ) ?>" class="button button-primary">
-            <span id="odwpasp-search_cancel_btn" href="<?php echo admin_url( 'tools.php?page=odwpasp-admin_page' ) ?>" class="button button-secondary"><?php _e( 'Cancel', 'odwpasp' ) ?></a>
+            <span id="odwpasp-search_cancel_btn" href="<?php echo admin_url( 'tools.php?page=odwpasp-admin_page' ) ?>" class="button button-secondary"><?php _e( 'Cancel', 'odwpasp' ) ?></span>
         </form>
     </div>
     <?php if( !$submitted_search ) : ?>
@@ -167,6 +191,7 @@ if( !function_exists( 'odwpasp_render_admin_page' ) ) :
     <form action="" method="POST">
         <input type="hidden" name="page" value="<?php echo ODWPASP_ADMIN_PAGE ?>">
         <input type="hidden" name="term" value="<?php echo $term ?>">
+        <?php wp_nonce_field( 'odwpasp_nonce' ) ?>
         <div class="form-container">
             <div class="form-container-inner">
                 <div class="form-cell-left">
@@ -229,6 +254,9 @@ if( !function_exists( 'odwpasp_add_meta_to_all_posts' ) ) :
      * Adds our priority meta key to all posts/pages.
      * @return void
      * @since 1.0.0
+     * @uses get_the_ID()
+     * @uses update_post_meta()
+     * @uses wp_reset_postdata()
      */
     function odwpasp_add_meta_to_all_posts() {
         $query = new WP_Query( array(
@@ -238,7 +266,7 @@ if( !function_exists( 'odwpasp_add_meta_to_all_posts' ) ) :
             'ignore_sticky_posts' => 1
         ) );
         
-        while ( $query->have_posts() ) {
+        while( $query->have_posts() ) {
             $query->the_post();
             $pid = get_the_ID();
 
@@ -277,6 +305,7 @@ if( !function_exists( 'odwpasp_pre_get_posts' ) ) :
      * @param WP_Query $q
      * @return void
      * @since 1.0.0
+     * @uses is_admin()
      */
     function odwpasp_pre_get_posts( $q ) {
         if( !is_admin() && $q->is_main_query() ) {
@@ -295,10 +324,11 @@ if( !function_exists( 'odwpasp_sort_by_priority' ) ) :
     /**
      * @global string $odwpasp_priority_sort
      * @global WP_Query $wp_query
+     * @link https://www.relevanssi.com/user-manual/relevanssi_hits_filter/
      * @param array $hits
      * @return array
      * @since 1.0.0
-     * @link https://www.relevanssi.com/user-manual/relevanssi_hits_filter/
+     * @uses get_post_meta()
      */
     function odwpasp_sort_by_priority( $hits ) {
         global $odwpasp_priority_sort, $wp_query;
@@ -361,6 +391,7 @@ if( !function_exists( 'odwpasp_register_meta_boxes' ) ) :
      * Register priority meta box.
      * @return void
      * @since 1.0.0
+     * @uses add_meta_box()
      */
     function odwpasp_register_meta_boxes() {
         add_meta_box(
@@ -380,14 +411,16 @@ if( !function_exists( 'odwpasp_priority_metabox_render' ) ) :
      * @param WP_Post $post
      * @return void
      * @since 1.0.0
-     * @todo Include `wponce()`!
+     * @uses wp_nonce_field()
      */
     function odwpasp_priority_metabox_render( $post ) {
         $priority = get_post_meta( $post->ID, ODWPASP_META_KEY, true );
+        wp_nonce_field( 'odwpasp_nonce' );
+
 ?>
 <p>
     <label for="odwpasp-priority"><?php _e( 'Priority:', 'odwpasp' ) ?></label>
-    <input class="short-text" id="odwpasp-priority" name="odwpasp-priority" type="text" value="<?php esc_attr( $priority ) ?>">
+    <input class="short-text" id="odwpasp-priority" name="odwpasp-priority" type="text" value="<?php echo esc_attr( $priority ) ?>">
 </p>
 <?php
     }
@@ -401,7 +434,10 @@ if( !function_exists( 'odwpasp_priority_metabox_save' ) ) :
      * @param WP_Post $post
      * @return void
      * @since 1.0.0
-     * @todo Include `wponce()`!
+     * @uses current_user_can()
+     * @uses wp_verify_nonce()
+     * @uses update_post_meta()
+     * 
      */
     function odwpasp_priority_metabox_save( $post_id, $post ) {
 
@@ -414,6 +450,12 @@ if( !function_exists( 'odwpasp_priority_metabox_save' ) ) :
         }
 
         if( 'post' != $post->post_type && 'page' != $post->post_type ) {
+            return $post_id;
+        }
+
+        $is_valid_nonce = ( isset( $_POST['odwpasp_nonce'] ) && wp_verify_nonce( $_POST['odwpasp_nonce'], ODWPASP_FILE ) );
+
+        if( !$is_valid_nonce ) {
             return $post_id;
         }
 
@@ -430,11 +472,11 @@ add_action( 'save_post', 'odwpasp_priority_metabox_save', 99, 2 );
 if( !function_exists( 'odwpasp_load_textdomain' ) ) :
     /**
      * Load plugin's localization.
-     * @link https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/
      * @param int $post_id
      * @param WP_Post $post
      * @return void
      * @since 1.0.0
+     * @uses load_plugin_textdomain()
      */
     function odwpasp_load_textdomain() {
         load_plugin_textdomain( 'odwpasp', false, 'odwp-add_search_priorities/languages/' );
